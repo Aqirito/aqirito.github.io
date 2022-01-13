a5418972a08d64225c9e255d022be7109887a2cb
Aqirito@github.com <aqil.rahmat99@gmail.com>
2022-01-13 14:23:00 +0800

    UPDATE: import template hero about and facts

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,20 @@
 <script context="module" lang="ts">
+  import About from '$lib/about/About.svelte';
+  import Facts from '$lib/facts/Facts.svelte';
 </script>
 
+
+<!-- ======= Hero Section ======= -->
+<section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+  <div class="hero-container" data-aos="fade-in">
+    <h1>Alex Smith</h1>
+    <p>I'm <span class="typed" data-typed-items="Designer, Developer, Freelancer, Photographer"></span></p>
+  </div>
+</section>
+<!-- End Hero -->
+
+
 <main id="main">
-  <h1>HELLO</h1>
+  <About/>
+  <Facts/>
 </main>
