a1a419ee0a945c8ef7f73677a4404b7b4b727373
Aqirito@github.com <aqil.rahmat99@gmail.com>
2022-01-13 16:45:04 +0800

    UPDATE: Import components

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,20 +1,32 @@
 <script context="module" lang="ts">
   import About from '$lib/about/About.svelte';
   import Facts from '$lib/facts/Facts.svelte';
+  import Skills from '$lib/skills/Skills.svelte';
+  import Resume from '$lib/resume/Resume.svelte';
+  import Portfolio from '$lib/portfolio/Portfolio.svelte';
+  import Services from '$lib/services/Services.svelte';
+  import Testimonials from '$lib/testimonials/Testimonials.svelte';
+  import Contact from '$lib/contact/Contact.svelte';
 </script>
-
 
 <!-- ======= Hero Section ======= -->
 <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
   <div class="hero-container" data-aos="fade-in">
-    <h1>Alex Smith</h1>
-    <p>I'm <span class="typed" data-typed-items="Designer, Developer, Freelancer, Photographer"></span></p>
+    <h1>Wan'z Hate'd</h1>
+    <p>I'm <span class="typed" data-typed-items="Software Developer, Casual Gamer"></span></p>
   </div>
 </section>
 <!-- End Hero -->
 
-
 <main id="main">
   <About/>
   <Facts/>
+  <Skills/>
+  <Resume/>
+  <Portfolio/>
+  <Services/>
+  <Testimonials/>
+  <Contact/>
 </main>
+
+<a href="#hero" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
