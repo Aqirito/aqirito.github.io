271af1201b9a6641094275817f046e7d1062d095
Aqirito@github.com <aqil.rahmat99@gmail.com>
2022-01-14 15:23:32 +0800

    UPDATE: my facts

--- src/lib/facts/Facts.svelte
+++ src/lib/facts/Facts.svelte
@@ -4,7 +4,8 @@
 
     <div class="section-title">
       <h2>Facts</h2>
-      <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+      <p>
+        I have participated in various technology -related projects in Sabah. My main goal is to improve the quality of life at the same time by making Sabah one of the main sources in terms of technology.      </p>
     </div>
 
     <div class="row no-gutters">
@@ -12,32 +13,32 @@
       <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up">
         <div class="count-box">
           <i class="bi bi-emoji-smile"></i>
-          <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="1" class="purecounter"></span>
-          <p><strong>Happy Clients</strong> consequuntur quae</p>
+          <span data-purecounter-start="0" data-purecounter-end="4" data-purecounter-duration="1" class="purecounter"></span>
+          <p><strong>Big Clients:</strong> Politeknik Kota Kinabalu (PKK), Kementerian Belia & Sukan (KBS), Ko-Nelayan, Marine Research Foundation (MRF), etc.</p>
         </div>
       </div>
 
       <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up" data-aos-delay="100">
         <div class="count-box">
           <i class="bi bi-journal-richtext"></i>
-          <span data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1" class="purecounter"></span>
-          <p><strong>Projects</strong> adipisci atque cum quia aut</p>
+          <span data-purecounter-start="0" data-purecounter-end="7" data-purecounter-duration="1" class="purecounter"></span>
+          <p><strong>Big Projects:</strong> Marine-PiEye, Platform E-Kan, PKK-BAMS, JHEWA E-Ledger, etc.</p>
         </div>
       </div>
 
       <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up" data-aos-delay="200">
         <div class="count-box">
           <i class="bi bi-headset"></i>
-          <span data-purecounter-start="0" data-purecounter-end="1453" data-purecounter-duration="1" class="purecounter"></span>
-          <p><strong>Hours Of Support</strong> aut commodi quaerat</p>
+          <span data-purecounter-start="0" data-purecounter-end="14520" data-purecounter-duration="1" class="purecounter"></span>
+          <p><strong>Hours Of Support</strong> Raw sum of two years experience</p>
         </div>
       </div>
 
       <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up" data-aos-delay="300">
         <div class="count-box">
           <i class="bi bi-people"></i>
-          <span data-purecounter-start="0" data-purecounter-end="32" data-purecounter-duration="1" class="purecounter"></span>
-          <p><strong>Hard Workers</strong> rerum asperiores dolor</p>
+          <span data-purecounter-start="0" data-purecounter-end="2040" data-purecounter-duration="1" class="purecounter"></span>
+          <p><strong>Github Contributions </strong> Github Based</p>
         </div>
       </div>
 
